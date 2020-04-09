114cc52818e1f4879b13a883f9772883a4824d66
jerrywu <1264135980@qq.com>
2020-04-09 16:40:12 +0800

    arch

--- arch/src/main/java/wbq/frame/arch/mvvm/SubViewController.java
+++ arch/src/main/java/wbq/frame/arch/mvvm/SubViewController.java
@@ -1,20 +1,27 @@
 package wbq.frame.arch.mvvm;
 
-import android.support.annotation.NonNull;
 import android.view.View;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 /**
  * Created by Jerry on 2019-09-11 20:22
  */
 public class SubViewController extends ViewController {
-    private final View mContentView;
+    private @Nullable final View mContentView;
 
-    public SubViewController(@NonNull ViewController parent, @NonNull View contentView) {
+    public SubViewController(@NonNull ViewController parent, @Nullable View contentView) {
         super(parent);
         mContentView = contentView;
     }
 
-    public View getContentView() {
+    public final View getContentView() {
         return mContentView;
+    }
+
+    public final <T extends View> T findViewById(@IdRes int id) {
+        return mContentView != null ? mContentView.findViewById(id) : null;
     }
 }
