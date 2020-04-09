dbccf8b3aafd4173b363efe390fb00a1ca4d0659
jerrywu <1264135980@qq.com>
2020-04-09 16:42:59 +0800

    http

--- network/src/main/java/wbq/frame/network/RetrofitHttpRequest.java
+++ network/src/main/java/wbq/frame/network/RetrofitHttpRequest.java
@@ -3,6 +3,7 @@ package wbq.frame.network;
 import java.lang.reflect.ParameterizedType;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -12,14 +13,18 @@ public abstract class RetrofitHttpRequest<T> extends HttpRequest {
 
     public abstract String getBaseUrl();
 
-    public T create() {
+    public final T create() {
         Class<T> service = (Class<T>)((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+        return retrofit().create(service);
+    }
+
+    protected Retrofit retrofit() {
         Retrofit retrofit = new Retrofit.Builder()
                 .client(getClient())
                 .baseUrl(getBaseUrl())
                 .addConverterFactory(GsonConverterFactory.create(getGson()))
-//                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
-        return retrofit.create(service);
+        return retrofit;
     }
 }
